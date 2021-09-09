b3f45dfd36fd2cbc7e5325335430d21f0146e5d6
Brett Fleming <brettfleming015@gmail.com>
2021-09-09 13:34:01 -0500

    added usestate and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] =useState()
+  useEffect(() => {
+    try {
+    } catch (error) {
+      
+    }
+
+  }, [])
   return (
     <div className="App">
+      
       <p>Hello world</p>
     </div>
   );
