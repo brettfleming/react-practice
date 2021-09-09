54223bfd855f895c6ca274357ce5eb118729bbb6
Brett Fleming <brettfleming015@gmail.com>
2021-09-09 15:14:50 -0500

    working on fecth funtion

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] =useState()
+  const [data, setData] =useState([])
   useEffect(() => {
-    try {
       fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=hard')
-       .then(response => {
-         if (response.ok) {
-           return response.json()
-           
-         } 
-       })
-    } catch (err) {
-      console.log(err)
-    }
-
+        .then(response => {
+          if (response.ok) {
+            return response.json 
+          } throw response;
+        })
+        .then(data => {
+          setData(data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
   }, [])
   return (
     <div className="App">
-      
+      {data}
       <p>Hello world</p>
     </div>
   );
