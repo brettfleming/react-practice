14394a575159c789733b0a202bc00a3c062a8b13
Brett Fleming <brettfleming015@gmail.com>
2021-09-09 15:51:00 -0500

    messed with api data a little bit

--- src/App.js
+++ src/App.js
@@ -19,11 +19,23 @@ function App() {
     <div className="App">
       {
         data.map(data => {
+          if (data.type === 'multiple') {
+            return (
+            <div key={Math.floor(Math.random() * 1000)}>
+              <p >{data.question}</p>
+              <p>{data.correct_answer}</p>
+              {
+              data.incorrect_answers.map(option => {
+                return <p>{option}</p>
+              })}
+            </div>
+            )
+          }
           return (
             <p key={Math.floor(Math.random() * 1000)}>{data.question}</p> )
         })
       }
-      <p>Hello world</p>
+      
     </div>
   );
 }
