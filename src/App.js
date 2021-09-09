9e6ed8583d879dcf4a0a03d6860b0ba6953c21dc
Brett Fleming <brettfleming015@gmail.com>
2021-09-09 15:32:32 -0500

    pulling data from the api and using it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
   const [data, setData] =useState([])
   useEffect(() => {
-      fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=hard')
+      axios('https://opentdb.com/api.php?amount=10&category=9&difficulty=hard')
         .then(response => {
-          if (response.ok) {
-            return response.json 
-          } throw response;
-        })
-        .then(data => {
-          setData(data)
+          console.log(response.data.results)
+          setData(response.data.results)
         })
         .catch(err => {
           console.log(err)
         })
   }, [])
+  console.log(data)
   return (
     <div className="App">
-      {data}
+      {
+        data.map(data => {
+          return (
+            <p key={Math.floor(Math.random() * 1000)}>{data.question}</p> )
+        })
+      }
       <p>Hello world</p>
     </div>
   );
